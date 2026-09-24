ab2bb2388a082eae7b1b18c31ae79bc732843904
Javier Diaz Urbano <me@xavidiaz.com>
2026-09-24 07:50:15 +0200

    just push

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -2,7 +2,7 @@ namespace TodoWarmup.Models;
 
 public class TodoItem
 {
-    public required int Id { get; set; }
+    public required Guid Id { get; set; }
     public required string Text { get; set; }
     public bool IsDone { get; set; } = false;
 }
